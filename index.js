690c30a5004c4aa4805386689749a494b92e9aba
BubbaCodes <44630260+BubbaCodes@users.noreply.github.com>
2018-11-10 21:03:53 -0500

    Resume
    Addition for Resume site.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*function openNav (){
+    document.getElementById("side-nav") .style.width="250px";
+    document.getElementsById("main") .style.marginRight="250px";
+}
+function closeNav (){
+    document.getElementById("side-nav") .style.width="0px";
+    document.getElementById("main") .style.marginRight="0px";
+}*/
